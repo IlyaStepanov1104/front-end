194f799dfc3cd0bc49f620be073812181ae2a635
IlyaStepanov1104 <102037915+IlyaStepanov1104@users.noreply.github.com>
2022-11-02 14:21:37 +0300

    add static files
    Добавлены статические файлы стилей, подключены к проекту

--- back/riddles/views.py
+++ back/riddles/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "index.html")
+    text = "Если ты не голубой, нарисуй вагон другой: "
+    image = "http://img2.reactor.cc/pics/post/full/anon-1483595.png"
+    alt = "ПОЕЗД"
+    return render(request, "index.html", {"post_text": text, "post_image": image, "post_image_alt": alt})
